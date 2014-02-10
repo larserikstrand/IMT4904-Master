161865a2e063c84341af880c8a8ece763709710b
Lars Erik Strand <strand.larserik@gmail.com>
2014-02-10 13:39:11 +0100

    project files added

---
+++ MTP/src/no/hig/strand/lars/mtp/Utilities.java
@@ -0,0 +1,11 @@
+package no.hig.strand.lars.mtp;
+
+public final class Utilities {
+
+	public Utilities() {}
+	
+	
+	public static void getAddress() {
+		
+	}
+}
---
+++ MTP/src/no/hig/strand/lars/mtp/WeekFragment.java
@@ -0,0 +1,18 @@
+package no.hig.strand.lars.mtp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WeekFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_today, container, false);
+		
+		return rootView;
+	}
+}
